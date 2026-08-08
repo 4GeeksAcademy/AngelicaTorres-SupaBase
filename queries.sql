71dc660a9bff9c0dd146b3babcb6baace67d7dad
Angelica Torres <yosoyangelicatorres@gmail.com>
2026-08-08 03:52:26 +0000

    Consulta y resultado 5

--- queries.sql
+++ queries.sql
@@ -15,4 +15,10 @@ SELECT * FROM enrollments
 where passed = false
 order by completion_percentage desc limit 5;
 
--- 5ta Consulta
+-- 5ta Consulta "Use el ultimo a;o como fecha 05-03-2024 al 05-03-2025 porque no hay nada despues de esa fecha"
+SELECT * 
+FROM enrollments
+WHERE enrollment_date::date BETWEEN '2024-03-05' AND '2025-03-05'
+ORDER BY enrollment_date DESC;
+
+-- 6ta Cosulta
