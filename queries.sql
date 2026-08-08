2577e4657ddc3ebd0ea6deead7464383883bbc7a
Angelica Torres <yosoyangelicatorres@gmail.com>
2026-08-08 03:35:15 +0000

    COnsulta y resultado 2

--- queries.sql
+++ queries.sql
@@ -3,3 +3,7 @@ SELECT student_name, student_email, completion_percentage FROM enrollments
 where course_title = 'Intro to Python'
 
 -- 2da consulta
+SELECT * FROM enrollments 
+where completion_percentage < 10
+
+-- 3ra consulta
